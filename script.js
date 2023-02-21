a1edf802c6ed2bebb4376f32618124c522fc51c0
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-02-21 22:15:17 +0600

    Add score

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ function playGame(){
     let randomNumber = Math.floor(Math.random(0)*10);
     let number_of_guess = 0;
     const guessLimit = 4;
+
+    let score = 0;
+    let highScore = 0;
+
+    while(number_of_guess <= guessLimit){
+        let guess = Number(prompt("Enter your guessed number:"));
+        number_of_guess++ ;
+        
+    }
 }
