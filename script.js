d6cf55e24f54fe985f345345fe331d1f3c349777
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-02-21 23:38:35 +0600

    link name from script to index

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("startGame").onclick = function() {playGame()};
 function playGame(){
 
     let userName = prompt("What's your name?");
+    document.getElementById("userName").innerHTML = userName;
 
     let randomNumber = Math.floor(Math.random(0)*10);
     let number_of_guess = 0;
