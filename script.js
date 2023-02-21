08c850db319c2bb1f62dd954781057fe45b15b5e
Aryan <125204827+aryancodespy@users.noreply.github.com>
2023-02-21 22:09:28 +0600

    Add function

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("startGame").onclick = function() {playGame()};
+
+function playGame(){
+
+    let randomNumber = Math.floor(Math.random(0)*10);
+    let number_of_guess = 0;
+    const guessLimit = 4;
+}
